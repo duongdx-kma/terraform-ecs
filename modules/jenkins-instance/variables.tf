6546197cab2d2f9a6e12ef40e05a591de51b4e56
duongdx <dinh.xuan.duong@sun-asterisk.com>
2023-12-11 09:30:52 +0700

    add jenkins instance

---
+++ modules/jenkins-instance/variables.tf
@@ -0,0 +1,24 @@
+variable user-data {}
+variable public-key {}
+variable ubuntu-ami {
+  type = string
+}
+
+variable instance-type {
+  type = string
+}
+
+variable instance-device-name {
+  type = string
+}
+
+variable jenkins-ingress {
+  type = list(object({
+    from_port: number
+    to_port: number
+    protocol: string
+    cidr_blocks: list(string)
+  }))
+
+  description = "jenkins ingress"
+}
