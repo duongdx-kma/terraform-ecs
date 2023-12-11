a4b14d81fabd8554d0052139eb1446071affbcd3
duongdx <dinh.xuan.duong@sun-asterisk.com>
2023-12-11 21:50:36 +0700

    add ssl/tls

--- dev/scripts/append.sh
+++ dev/scripts/append.sh
@@ -6,6 +6,6 @@ module "auto-scaling" {
   source      = "../modules/auto-scaling"
   ecs_cluster = module.ecs.ecs_cluster
   ecs_service = module.ecs.ecs_service
-  depends_on  = [module.ecs]
+  depends_on  = [module.ecs.ecs_cluster, module.ecs.ecs_service]
 }
 EOF
